1e41aee085654be817860d92461de7374fd2820e
Stephen Nelson <stephen@eccostudio.com>
2017-10-07 11:53:22 +0100

    Initial welcome screen added

---
+++ game.py
@@ -0,0 +1,15 @@
+print("\n" * 100)
+print('Welcome to Star Wars Adventure')
+print('==============================')
+print("\n")
+
+print('You managed to escape Scarif before it was engulfed in a Tsunami.')
+print('You are drifting in an escape pod in outer space. What do you want to do?')
+
+command = raw_input('? ')
+
+if command == 'north':
+  print('Go North')
+elif command == 'south':
+  print('Go south')
+ 
